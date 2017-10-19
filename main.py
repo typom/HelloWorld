c9bdc3d7931c679b062a9e7e7bd9b1e81098080c
typom <32912186+typom@users.noreply.github.com>
2017-10-18 21:47:36 -0400

    creation of sub1

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("Hello World!")
 print("my first change")
+print("my second change")
---
+++ sub1.py
@@ -0,0 +1,8 @@
+'''
+Created on Oct 18, 2017
+
+@author: Alexandre
+'''
+
+if __name__ == '__main__':
+    print("do nothing")
